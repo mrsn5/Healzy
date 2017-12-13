aca505942a8228469907dfa9dc7a9215628df389
San Nguyen <mr.sn5.kma@gmail.com>
2017-12-13 17:23:19 +0200

    bot added

---
+++ Backend/Calorizator/products.js
@@ -0,0 +1,582 @@
+/**
+ * Created by sannguyen on 13.12.17.
+ */
+
+var products_info = [
+    {
+        id: 2,
+        title: 'Антильский огурец',
+        proteins: 1.8,
+        fats: 1.3,
+        carbohydrates: 7.6,
+        calories: 44.0,
+        category: 'vegetables'
+    },
+    {
+        id: 7,
+        title: 'Арракача',
+        proteins: 0.9,
+        fats: 0.1,
+        carbohydrates: 17.1,
+        calories: 60.0,
+        category: 'vegetables'
+    },
+    {
+        id: 12,
+        title: 'Артишок',
+        proteins: 1.2,
+        fats: 0.1,
+        carbohydrates: 6.0,
+        calories: 28.0,
+        category: 'vegetables'
+    },
+    {
+        id: 21,
+        title: 'Ассорти Бондюэль из сладкого перца',
+        proteins: 1.1,
+        fats: 0.2,
+        carbohydrates: 4.8,
+        calories: 25.0,
+        category: 'vegetables'
+    },
+    {
+        id: 34,
+        title: 'Ассорти Бондюэль из фасоли с ананасами в соусе Карри',
+        proteins: 3.8,
+        fats: 1.1,
+        carbohydrates: 9.3,
+        calories: 62.0,
+        category: 'vegetables'
+    },
+    {
+        id: 46,
+        title: 'Ассорти Бондюэль из цветной капусты, романеско и брокколи',
+        proteins: 1.8,
+        fats: 2.7,
+        carbohydrates: 8.6,
+        calories: 66.0,
+        category: 'vegetables'
+    },
+    {
+        id: 51,
+        title: 'Баклажан',
+        proteins: 1.2,
+        fats: 0.1,
+        carbohydrates: 4.5,
+        calories: 24.0,
+        category: 'vegetables'
+    },
+    {
+        id: 57,
+        title: 'Баклажан консервированный',
+        proteins: 0.9,
+        fats: 0.7,
+        carbohydrates: 7.3,
+        calories: 49.0,
+        category: 'vegetables'
+    },
+    {
+        id: 64,
+        title: 'Баклажанная икра консервированная',
+        proteins: 1.7,
+        fats: 13.3,
+        carbohydrates: 5.1,
+        calories: 148.0,
+        category: 'vegetables'
+    },
+    {
+        id: 71,
+        title: 'Баклажанная икра свежая',
+        proteins: 0.6,
+        fats: 7.0,
+        carbohydrates: 6.0,
+        calories: 90.0,
+        category: 'vegetables'
+    },
+    {
+        id: 77,
+        title: 'Бамбук листья',
+        proteins: 2.0,
+        fats: 0.0,
+        carbohydrates: 4.0,
+        calories: 32.0,
+        category: 'vegetables'
+    },
+    {
+        id: 84,
+        title: 'Бамбук побеги вареные',
+        proteins: 1.5,
+        fats: 0.2,
+        carbohydrates: 0.9,
+        calories: 12.0,
+        category: 'vegetables'
+    },
+    {
+        id: 91,
+        title: 'Бамбук побеги консервированные',
+        proteins: 1.7,
+        fats: 0.4,
+        carbohydrates: 1.8,
+        calories: 19.0,
+        category: 'vegetables'
+    },
+    {
+        id: 98,
+        title: 'Бамбук побеги сырые',
+        proteins: 2.6,
+        fats: 0.3,
+        carbohydrates: 3.0,
+        calories: 27.0,
+        category: 'vegetables'
+    },
+    {
+        id: 103,
+        title: 'Бамия',
+        proteins: 2.0,
+        fats: 0.1,
+        carbohydrates: 3.8,
+        calories: 25.0,
+        category: 'vegetables'
+    },
+    {
+        id: 113,
+        title: 'Бедренец камнеломка',
+        proteins: 0.2,
+        fats: 0.0,
+        carbohydrates: 5.0,
+        calories: 27.0,
+        category: 'vegetables'
+    },
+    {
+        id: 118,
+        title: 'Бобы',
+        proteins: 6.0,
+        fats: 0.1,
+        carbohydrates: 8.5,
+        calories: 57.0,
+        category: 'vegetables'
+    },
+    {
+        id: 124,
+        title: 'Бобы зелёные',
+        proteins: 6.0,
+        fats: 0.1,
+        carbohydrates: 10.5,
+        calories: 60.0,
+        category: 'vegetables'
+    },
+    {
+        id: 129,
+        title: 'Брюква',
+        proteins: 1.2,
+        fats: 0.1,
+        carbohydrates: 7.7,
+        calories: 37.0,
+        category: 'vegetables'
+    },
+    {
+        id: 135,
+        title: 'Брюква запечённая',
+        proteins: 2.1,
+        fats: 1.6,
+        carbohydrates: 7.5,
+        calories: 51.0,
+        category: 'vegetables'
+    },
+    {
+        id: 141,
+        title: 'Брюква тушеная',
+        proteins: 1.2,
+        fats: 8.5,
+        carbohydrates: 8.6,
+        calories: 114.0,
+        category: 'vegetables'
+    },
+    {
+        id: 146,
+        title: 'Вигна',
+        proteins: 3.0,
+        fats: 0.4,
+        carbohydrates: 18.8,
+        calories: 90.0,
+        category: 'vegetables'
+    },
+    {
+        id: 152,
+        title: 'Горох варёный',
+        proteins: 6.0,
+        fats: 0.0,
+        carbohydrates: 9.0,
+        calories: 60.0,
+        category: 'vegetables'
+    },
+    {
+        id: 159,
+        title: 'Горох колотый Агро-Альянс',
+        proteins: 23.0,
+        fats: 1.0,
+        carbohydrates: 62.0,
+        calories: 350.0,
+        category: 'vegetables'
+    },
+    {
+        id: 165,
+        title: 'Горох лущеный',
+        proteins: 23.0,
+        fats: 1.6,
+        carbohydrates: 57.7,
+        calories: 323.0,
+        category: 'vegetables'
+    },
+    {
+        id: 171,
+        title: 'Горох маш',
+        proteins: 23.5,
+        fats: 2.0,
+        carbohydrates: 46.0,
+        calories: 300.0,
+        category: 'vegetables'
+    },
+    {
+        id: 178,
+        title: 'Горох маш (ростки)',
+        proteins: 3.1,
+        fats: 0.2,
+        carbohydrates: 6.0,
+        calories: 30.0,
+        category: 'vegetables'
+    },
+    {
+        id: 186,
+        title: 'Горох маш Ярмарка Платинум',
+        proteins: 23.5,
+        fats: 2.0,
+        carbohydrates: 50.0,
+        calories: 312.0,
+        category: 'vegetables'
+    },
+    {
+        id: 193,
+        title: 'Горох Мистраль Айдахо',
+        proteins: 20.8,
+        fats: 1.4,
+        carbohydrates: 55.2,
+        calories: 317.0,
+        category: 'vegetables'
+    },
+    {
+        id: 200,
+        title: 'Горох Мистраль Орегон',
+        proteins: 20.0,
+        fats: 1.5,
+        carbohydrates: 66.4,
+        calories: 327.0,
+        category: 'vegetables'
+    },
+    {
+        id: 207,
+        title: 'Горох Пассим колотый',
+        proteins: 23.0,
+        fats: 1.6,
+        carbohydrates: 48.1,
+        calories: 299.0,
+        category: 'vegetables'
+    },
+    {
+        id: 213,
+        title: 'Горох сушёный',
+        proteins: 20.5,
+        fats: 2.0,
+        carbohydrates: 53.3,
+        calories: 298.0,
+        category: 'vegetables'
+    },
+    {
+        id: 219,
+        title: 'Горошек зелёный',
+        proteins: 5.0,
+        fats: 0.2,
+        carbohydrates: 13.8,
+        calories: 73.0,
+        category: 'vegetables'
+    },
+    {
+        id: 249,
+        title: 'Горошек зелёный',
+        proteins: 6.4,
+        fats: 0.4,
+        carbohydrates: 10.5,
+        calories: 83.0,
+        category: 'vegetables'
+    },
+    {
+        id: 257,
+        title: 'Горошек зелёный Бондюэль замороженный',
+        proteins: 6.9,
+        fats: 0.4,
+        carbohydrates: 19.4,
+        calories: 85.0,
+        category: 'vegetables'
+    },
+    {
+        id: 265,
+        title: 'Горошек зелёный Бондюэль Нежный',
+        proteins: 4.8,
+        fats: 0.2,
+        carbohydrates: 7.5,
+        calories: 52.0,
+        category: 'vegetables'
+    },
+    {
+        id: 275,
+        title: 'Горошек зелёный Бондюэль с молодой морковью',
+        proteins: 3.3,
+        fats: 0.5,
+        carbohydrates: 9.3,
+        calories: 54.0,
+        category: 'vegetables'
+    },
+    {
+        id: 286,
+        title: 'Горошек зелёный Бондюэль с морковью в кубиках',
+        proteins: 3.0,
+        fats: 0.2,
+        carbohydrates: 10.0,
+        calories: 53.0,
+        category: 'vegetables'
+    },
+    {
+        id: 293,
+        title: 'Горошек зелёный замороженный',
+        proteins: 6.4,
+        fats: 0.4,
+        carbohydrates: 16.3,
+        calories: 72.0,
+        category: 'vegetables'
+    },
+    {
+        id: 302,
+        title: 'Горошек зелёный Каждый день замороженный',
+        proteins: 5.0,
+        fats: 0.2,
+        carbohydrates: 8.5,
+        calories: 55.0,
+        category: 'vegetables'
+    },
+    {
+        id: 309,
+        title: 'Горошек зелёный консервированный',
+        proteins: 3.6,
+        fats: 0.1,
+        carbohydrates: 9.8,
+        calories: 55.0,
+        category: 'vegetables'
+    },
+    {
+        id: 315,
+        title: 'Гулявник лекарственный',
+        proteins: 0.3,
+        fats: 0.0,
+        carbohydrates: 5.0,
+        calories: 21.0,
+        category: 'vegetables'
+    },
+    {
+        id: 320,
+        title: 'Дайкон',
+        proteins: 1.2,
+        fats: 0.0,
+        carbohydrates: 4.1,
+        calories: 21.0,
+        category: 'vegetables'
+    },
+    {
+        id: 325,
+        title: 'Девясил',
+        proteins: 2.9,
+        fats: 0.0,
+        carbohydrates: 0.2,
+        calories: 15.0,
+        category: 'vegetables'
+    },
+    {
+        id: 330,
+        title: 'Зелень',
+        proteins: 2.6,
+        fats: 0.4,
+        carbohydrates: 5.2,
+        calories: 36.0,
+        category: 'vegetables'
+    },
+    {
+        id: 340,
+        title: 'Зразы картофельные от Ильиной с грибами',
+        proteins: 2.0,
+        fats: 1.0,
+        carbohydrates: 13.0,
+        calories: 70.0,
+        category: 'vegetables'
+    },
+    {
+        id: 350,
+        title: 'Зразы картофельные от Ильиной с капустой',
+        proteins: 3.3,
+        fats: 3.9,
+        carbohydrates: 15.0,
+        calories: 108.0,
+        category: 'vegetables'
+    },
+    {
+        id: 375,
+        title: 'Кабачки',
+        proteins: 1.1,
+        fats: 6.0,
+        carbohydrates: 6.7,
+        calories: 88.0,
+        category: 'vegetables'
+    },
+    {
+        id: 381,
+        title: 'Кабачок консервированный',
+        proteins: 0.4,
+        fats: 0.1,
+        carbohydrates: 4.9,
+        calories: 22.0,
+        category: 'vegetables'
+    },
+    {
+        id: 386,
+        title: 'Каперсы',
+        proteins: 0.8,
+        fats: 0.1,
+        carbohydrates: 2.6,
+        calories: 14.0,
+        category: 'vegetables'
+    },
+    {
+        id: 392,
+        title: 'Каперсы консервированные',
+        proteins: 2.4,
+        fats: 0.9,
+        carbohydrates: 1.7,
+        calories: 24.0,
+        category: 'vegetables'
+    },
+    {
+        id: 398,
+        title: 'Каперсы маринованные',
+        proteins: 2.4,
+        fats: 0.9,
+        carbohydrates: 1.7,
+        calories: 24.0,
+        category: 'vegetables'
+    },
+    {
+        id: 404,
+        title: 'Капуста белокочанная',
+        proteins: 1.8,
+        fats: 0.1,
+        carbohydrates: 4.7,
+        calories: 27.0,
+        category: 'vegetables'
+    },
+    {
+        id: 411,
+        title: 'Капуста белокочанная жареная',
+        proteins: 1.8,
+        fats: 2.8,
+        carbohydrates: 4.2,
+        calories: 49.0,
+        category: 'vegetables'
+    },
+    {
+        id: 418,
+        title: 'Капуста белокочанная сушеная',
+        proteins: 15.0,
+        fats: 1.4,
+        carbohydrates: 48.0,
+        calories: 278.0,
+        category: 'vegetables'
+    },
+    {
+        id: 424,
+        title: 'Капуста брокколи',
+        proteins: 3.0,
+        fats: 0.4,
+        carbohydrates: 5.2,
+        calories: 28.0,
+        category: 'vegetables'
+    },
+    {
+        id: 454,
+        title: 'Капуста брокколи',
+        proteins: 3.0,
+        fats: 0.4,
+        carbohydrates: 4.0,
+        calories: 27.0,
+        category: 'vegetables'
+    },
+    {
+        id: 461,
+        title: 'Капуста брокколи замороженная',
+        proteins: 2.7,
+        fats: 0.4,
+        carbohydrates: 4.7,
+        calories: 24.0,
+        category: 'vegetables'
+    },
+    {
+        id: 467,
+        title: 'Капуста брюссельская',
+        proteins: 4.8,
+        fats: 0.0,
+        carbohydrates: 8.0,
+        calories: 43.0,
+        category: 'vegetables'
+    },
+    {
+        id: 497,
+        title: 'Капуста брюссельская',
+        proteins: 4.5,
+        fats: 0.5,
+        carbohydrates: 8.4,
+        calories: 36.0,
+        category: 'vegetables'
+    },
+    {
+        id: 503,
+        title: 'Капуста кале',
+        proteins: 3.3,
+        fats: 0.7,
+        carbohydrates: 8.0,
+        calories: 50.0,
+        category: 'vegetables'
+    },
+    {
+        id: 509,
+        title: 'Капуста квашеная',
+        proteins: 1.8,
+        fats: 0.1,
+        carbohydrates: 4.4,
+        calories: 19.0,
+        category: 'vegetables'
+    },
+    {
+        id: 515,
+        title: 'Капуста кольраби',
+        proteins: 2.8,
+        fats: 0.0,
+        carbohydrates: 10.7,
+        calories: 42.0,
+        category: 'vegetables'
+    },
+    {
+        id: 521,
+        title: 'Капуста консервированная',
+        proteins: 1.3,
+        fats: 0.1,
+        carbohydrates: 3.6,
+        calories: 23.0,
+        category: 'vegetables'
+    }
+];
---
+++ Backend/HealzyBot/index.js
@@ -0,0 +1,87 @@
+/**
+ * Created by sannguyen on 12.12.17.
+ */
+
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = "503726174:AAHgyg44GQ_YY-RqA4Pm70YT8lFnSA_Zg6c";
+const bot = new TelegramBot(TOKEN, {
+    polling: true
+});
+
+var DBFollowers = require("./db_followers");
+var db_followers = DBFollowers.init();
+var Follower = DBFollowers.getModel();
+
+var DBCalorizator = require("../Calorizator/calorizator");
+var db_calorizator = DBCalorizator.init();
+var Product = DBCalorizator.getModel();
+
+
+bot.onText(/\/start/, function (msg) {
+    const text = "Привіт, " + msg.from.first_name + "!\nЯ Healzy, твій персональний тренер.";
+    bot.sendMessage(msg.chat.id, text);
+    console.log(msg);
+    new Followers({
+        first_name: msg.chat.first_name,
+        last_name: msg.chat.last_name,
+        id: msg.chat.id
+    }).save(function (err) {
+        if(err) console.log(err);
+    });
+})
+
+bot.onText(/[\s\S]*/, function (msg) {
+    console.log(msg);
+})
+
+
+bot.onText(/\/send/, function (msg) {
+    if (msg.chat.id === 337800229){
+        const text = msg.text.substring(6);
+        Follower.find(function (err, follower) {
+            if (!err)
+                follower.forEach(function (follower) {
+                    bot.sendMessage(follower.id, text);
+                })
+        });
+    }
+});
+
+bot.onText(/\/add/, function (msg) {
+    if (msg.chat.id === 337800229){
+        const text = msg.text.substring(5);
+        new Product({title: text}).save();
+    }
+});
+
+bot.onText(/\/info/, function (msg) {
+    var text = msg.text.substring(5);
+    text = text.trim();
+    console.log(text);
+    Product.find(
+        {
+            title: text
+        }, function (err, product) {
+            var html;
+            if(!err && product.length != 0) {
+                product.forEach(function (p) {
+                    html =
+                        '<b>Калорії</b> : <i>' + p.calories + '</i> '+
+                        '<b>Жири</b> : <i>' + p.fats + '</i> '+
+                        '<b>Вуглеводи</b> : <i>' + p.carbohydrates + '</i> '+
+                        '<b>Білки</b> : <i>' + p.proteins + '</i> ';
+                    bot.sendMessage(msg.chat.id, html, {
+                        parse_mode: 'HTML'
+                    });
+                });
+            } else {
+                bot.sendMessage(msg.chat.id, "Вибач, але я не знаю такого продукта");
+            }
+        });
+});
+
+
+
+//381008276 - Anne
+//https://api.telegram.org/bot503726174:AAHgyg44GQ_YY-RqA4Pm70YT8lFnSA_Zg6c/sendMessage?chat_id=@337800229,text=тест.
+
