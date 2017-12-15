230d87ed5f2e52f25b2878d9a417af4a0fc96612
AnneManzhura <manzhurik@gmail.com>
2017-12-15 11:52:49 +0200

    sum NaN fixed but trouble remained

--- Frontend/www/assets/js/main.js
+++ Frontend/www/assets/js/main.js
@@ -87,24 +87,8 @@ var Storage = require('./LocalStorage');
 
 var Product_List=[];
 var allProducts=[];
-var sum=
-    {
-        mass: parseFloat(0),
-        proteins:parseFloat(0),
-        fats: parseFloat(0),
-        carbs: parseFloat(0),
-        calories: parseFloat(0)
-    }
-;
-var sumPer100g=
-    {
-        mass: parseFloat(100),
-        proteins:parseFloat(0),
-        fats: parseFloat(0),
-        carbs: parseFloat(0),
-        calories: parseFloat(0)
-    }
-;
+var sum=[];
+var sumPer100g= [];
 var counter;
 
 var $inputProduct =$('#automplete');
@@ -122,37 +106,27 @@ function addOneProduct(product, mass) {
 
 
     Product_List.push(new WrappedProduct(product, mass));
-    Product_List.forEach(calculateSum);
 
     updateList();
 }
 function removeFromList(wrappedProduct) {
-    sum.mass-=wrappedProduct.mass;
-    sum.proteins-=(wrappedProduct.product.proteins * wrappedProduct.mass * 0.01).toFixed(2);
-    sum.fats-=(wrappedProduct.product.fats * wrappedProduct.mass * 0.01).toFixed(2);
-    sum.carbs-=(wrappedProduct.product.carbs * wrappedProduct.mass * 0.01).toFixed(2);
-    sum.calories-=(wrappedProduct.product.calories * wrappedProduct.mass * 0.01).toFixed(2);
-
-    sumPer100g.proteins-=wrappedProduct.product.proteins;
-    sumPer100g.fats-=wrappedProduct.product.fats;
-    sumPer100g.carbs-=wrappedProduct.product.carbs;
-    sumPer100g.calories-=wrappedProduct.product.calories;
-
     Product_List.splice(Product_List.indexOf(wrappedProduct),1);
     updateList();
 }
 
 function calculateSum(wrappedProduct){
+    console.log(sum.mass);
     sum.mass= parseFloat(sum.mass) +  parseFloat(wrappedProduct.mass);
-    sum.proteins= parseFloat(sum.proteins)   +  parseFloat(wrappedProduct.product.proteins * wrappedProduct.mass * 0.01).toFixed(2);
-    sum.fats= parseFloat(sum.fats)   + parseFloat(wrappedProduct.product.fats * wrappedProduct.mass * 0.01).toFixed(2);
-    sum.carbs= parseFloat(sum.carbs)   + parseFloat(wrappedProduct.product.carbs * wrappedProduct.mass * 0.01).toFixed(2);
-    sum.calories= parseFloat(sum.calories)   + parseFloat(wrappedProduct.product.calories * wrappedProduct.mass * 0.01).toFixed(2);
+    sum.proteins= (parseFloat(sum.proteins)   +  parseFloat(wrappedProduct.product.proteins * wrappedProduct.mass * 0.01)).toFixed(2);
+    sum.fats= (parseFloat(sum.fats)   + parseFloat(wrappedProduct.product.fats * wrappedProduct.mass * 0.01)).toFixed(2);
+    sum.carbs= (parseFloat(sum.carbs)   + parseFloat(wrappedProduct.product.carbohydrates * wrappedProduct.mass * 0.01)).toFixed(2);
+    sum.calories= (parseFloat(sum.calories)   + parseFloat(wrappedProduct.product.calories * wrappedProduct.mass * 0.01)).toFixed(2);
 
-    sumPer100g.proteins=parseFloat(sumPer100g.proteins) +parseFloat(wrappedProduct.product.proteins);
-    sumPer100g.fats=parseFloat(sumPer100g.fats) + parseFloat(wrappedProduct.product.fats);
-    sumPer100g.carbs=parseFloat(sumPer100g.carbs) + parseFloat(wrappedProduct.product.carbs);
-    sumPer100g.calories=parseFloat(sumPer100g.calories) + parseFloat(wrappedProduct.product.calories);
+    sumPer100g.proteins=(parseFloat(sumPer100g.proteins) +parseFloat(wrappedProduct.product.proteins)).toFixed(2);
+    sumPer100g.fats=(parseFloat(sumPer100g.fats) + parseFloat(wrappedProduct.product.fats)).toFixed(2);
+    sumPer100g.carbs=(parseFloat(sumPer100g.carbs) + parseFloat(wrappedProduct.product.carbohydrates)).toFixed(2);
+    sumPer100g.calories=(parseFloat(sumPer100g.calories) + parseFloat(wrappedProduct.product.calories)).toFixed(2);
+
 }
 
 function updateList(){
@@ -180,10 +154,10 @@ function updateList(){
         $('#table_normal').append($node_normal);
     }
 
-    Storage.set("list",	Product_List);
+    Storage.set("list",Product_List);
 
     Product_List.forEach(showOnePizzaInCart);
-
+    Product_List.forEach(calculateSum);
 
     $('.weight_total').text(sum.mass);
     $('.proteins_total').text(sum.proteins);
@@ -215,16 +189,34 @@ function initCalc() {
             $inputProduct.autocomplete({
                 minLength:2,
                 delay:100,
+                autoFocus: true,
                 source: titles
             });
         }
     });
+    sum=
+        {
+            mass: parseFloat(0),
+            proteins:parseFloat(0),
+            fats: parseFloat(0),
+            carbs: parseFloat(0),
+            calories: parseFloat(0)
+        };
+
+    sumPer100g=
+        {
+            mass: parseFloat(100),
+            proteins:parseFloat(0),
+            fats: parseFloat(0),
+            carbs: parseFloat(0),
+            calories: parseFloat(0)
+        };
+
 
     var saved_products = Storage.get("list");
-    if (saved_products) {
+    if (saved_products)
         Product_List = saved_products;
-        Product_List.forEach(calculateSum);
-    }
+
     updateList();
 }
 
@@ -232,7 +224,7 @@ $(".addProduct").click(function () {
     if($inputProduct.val().trim()!=="" && $inputMass.val().trim()!==""){
         for(i=0; i<allProducts.length; i++){
             if($inputProduct.val().trim()===allProducts[i].title){
-                addOneProduct(allProducts[i],$inputMass.val());
+                addOneProduct(allProducts[i], parseFloat($inputMass.val()));
                 $inputProduct.val("");
                 $inputMass.val("");
             }
